6da2c695cf31c7b686d00d936a552b7877dcb8da
Opilane TTHK <opilane@tthk.ee>
2020-12-11 09:41:50 +0200

    first commit

---
+++ Frozen/Character.cs
@@ -0,0 +1,20 @@
+﻿namespace Frozen
+{
+    internal class Character
+    {
+        private int id;
+        private string title;
+        private long gross;
+
+        public Character(int id, string title, long gross)
+        {
+            this.id = id;
+            this.title = title;
+            this.gross = gross;
+        }
+
+        public static object Id { get; internal set; }
+        public static object Title { get; internal set; }
+        public static object LifeTimeGross { get; internal set; }
+    }
+}
---
+++ Frozen/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class Characters
+        {
+            class FrozenCharacters
+            {
+                string character;
+                string title;
+                public FrozenCharacters(string _title, string _character)
+                {
+                    character = _character;
+                    title = _title;
+                }
+                public string Character { get { return character; } }
+                public string Title { get { return title; } }
+            }
+            class FrozenList
+            {
+                List<FrozenCharacters> frozens;
+                public FrozenList()
+                {
+                    frozens = new List<FrozenCharacters>();
+                }
+                public void AddFrozentoList(string title, string character)
+                {
+                    FrozenCharacters newFrozen = new FrozenCharacters(title, character);
+                    frozens.Add(newFrozen);
+                }
+                public void PrintAllFrozen()
+                {
+                    foreach (FrozenCharacters character in frozens)
+                    {
+                        Console.WriteLine($"{character.Character} wants {character.Title} for christmas ");
+                    }
+                }
+            }
+            static void Main(string[] args)
+            {
+                string filePath = @"C:\Users\opilane\samples";
+                string fileName = @"Frozen.txt";
+                string fullFilePath = Path.Combine(filePath, fileName);
+                string[] linesFromfile = File.ReadAllLines(fullFilePath);            
+                FrozenList myFrozens = new FrozenList();
+                foreach (string line in linesFromfile)
+                {
+                    string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                    string frozenTitle = tempArray[1];
+                    string frozenCharacter = tempArray[0];
+
+                    myFrozens.AddFrozentoList(frozenTitle, frozenCharacter);
+                }
+                myFrozens.PrintAllFrozen();
+
+            }
+        }
+    }
+}
